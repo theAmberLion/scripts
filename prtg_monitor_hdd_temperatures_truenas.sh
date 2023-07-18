a4affc83de4e409dd8aa56b5314254ba7aa91101
theAmberLion <90892959+theAmberLion@users.noreply.github.com>
2023-07-18 13:34:05 +0300

    Update and rename prtg_monitor_hdd_temperatures.sh to prtg_monitor_hdd_temperatures_truenas.sh

--- prtg_monitor_hdd_temperatures_truenas.sh
+++ prtg_monitor_hdd_temperatures_truenas.sh
@@ -6,7 +6,7 @@ arrVar=()
 
 echo -n "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
 
-for i in 0 1 2 3 4 5
+for i in 0 1 2 3 4 5 		#change the "for loop" as you want/need
 do
 	arrVar+=( "$(smartctl -a /dev/"ada$i" | grep Temp | awk '{print $10}')" )
 	echo -n "<result><channel>ada$i</channel><value>${arrVar[$i]}</value><unit>°C</unit><LimitMode>1</LimitMode><LimitMaxWarning>55</LimitMaxWarning><LimitMaxError>60</LimitMaxError></result>"
